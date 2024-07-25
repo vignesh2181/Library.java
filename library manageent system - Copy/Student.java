eee950099a8c49b0e822430f7f9d72700293df11
vignesh2181 <146431678+vignesh2181@users.noreply.github.com>
2024-07-25 10:20:32 +0530

    Add files via upload

---
+++ library manageent system - Copy/Student.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+ 
+// Class
+public class Student 
+{
+ 
+    // Class member variables
+    String studentName;
+    String regNum;
+ 
+    book borrowedBooks[] = new book[3];
+    public int booksCount = 0;
+ 
+    // Creating object of Scanner class to
+    // take input from user
+    Scanner input = new Scanner(System.in);
+ 
+    // Constructor
+    public Student()
+    {
+        // Print statement
+        System.out.println("Enter Student Name:");
+ 
+        // This keywords refers to current instance
+        this.studentName = input.nextLine();
+ 
+        // Print statement
+        System.out.println("Enter Registration Number:");
+        this.regNum = input.nextLine();
+    }
+}
